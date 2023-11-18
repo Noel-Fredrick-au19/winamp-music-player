9e4ebdb58090c50234c6d9302f72f85c0e982fd5
noel.fredrick <noel.fredrick@cheersin.com>
2023-11-18 21:20:46 +0530

    first commit changes

---
+++ src/components/ExtraControls.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+
+import React from "react";
+
+// eslint-disable-next-line react/prop-types
+const ExtraControls = ({ children }) => {
+    return <div className="extraControls">{children}</div>;
+};
+
+export default ExtraControls;
